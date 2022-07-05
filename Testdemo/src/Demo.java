3a418596592a7cbfc20329767842d33f86885157
AN574BV <AN574BV@IN3072595W1.ey.net>
2022-07-05 10:57:54 +0530

    this is my second commit

--- Testdemo/src/Demo.java
+++ Testdemo/src/Demo.java
@@ -3,6 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("hi this is jasmi my first project");
+		System.out.println("hi this is jasmi my second project");
 
 	}
 
